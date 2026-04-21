97c360529b7cca27195216b5f001b1fe1969ddf3
Santiago <santiagp.riverapalomino@gmail.com>
2026-04-21 15:27:28 -0500

    deshabilitar dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  darkMode: ['class', '[data-theme="dark"]'],
-  content: [
+darkMode: 'selector',  content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
